0a4fd5c8ff4f4401c8c7fdc97b73fc8b47a5c4cb
Zaid <zaidasif011@gmail.com>
2025-08-07 00:29:23 +0500

    profile and createCommunity directory approach optimization

--- Frontend/TheSocial/app/home/(tabs)/createCommunity/_layout.tsx
+++ Frontend/TheSocial/app/home/(tabs)/createCommunity/_layout.tsx
@@ -1,12 +1,19 @@
 import { useEffect } from 'react';
 import { useModal } from '@/components/ModalContext';
+import { useRouter, usePathname } from 'expo-router';
 
 export default function CreateCommunityTabLayout() {
   const { openModal } = useModal();
+  const router = useRouter();
+  const pathname = usePathname();
 
-  useEffect(() => {
-    openModal('createCommunity');
-  }, [openModal]);
+  // useEffect(() => {
+    if (pathname === '/home/createCommunity') {
+      router.replace('/home/(tabs)/messages');
+      openModal('createCommunity');
+    }
+    console.log(pathname);
+  // }, [pathname]);
 
   return null;
 }
--- Frontend/TheSocial/app/home/(tabs)/profile/_layout.tsx
+++ Frontend/TheSocial/app/home/(tabs)/profile/_layout.tsx
@@ -1,12 +1,13 @@
-import { useRouter } from 'expo-router';
-import { useEffect } from 'react';
+import { useRouter, usePathname } from 'expo-router';
 
 export default function ProfileStack() {
   const router = useRouter();
+  const pathname = usePathname();
 
-  useEffect(() => {
+  if (pathname === '/home/profile') {
+    router.replace('/home/(tabs)/messages');
     router.push('/profile');
-  }, [router]);
+  }
 
   return null;
 }
